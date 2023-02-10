2e992fe00a2a8a8cf6149f3717f2aacad0fb496b
ZeT1an <584633819@qq.com>
2023-02-11 01:01:31 +0800

    fix: v13;

--- demo-project/demo.ts
+++ demo-project/demo.ts
@@ -15,4 +15,19 @@ function printPerson(person: IPerson) {
 }
 const kobe = { name: "kobe", age: 30, height: 1.98 }
 printPerson(kobe)
+
+
+class Point<T = number> {
+  x: T
+  y: T
+  constructor(x: T, y: T) {
+    this.x = x
+    this.y = y
+  }
+}
+
+const p1 = new Point(10, 20)
+console.log(p1.x)
+const p2 = new Point("123", "321")
+console.log(p2.x)
 export {}
