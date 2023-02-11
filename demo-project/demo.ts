59a6591a3b2f3e46c9bf5e4f5a1adede20cad546
ZeT1an <zetian.zhu@cimc.com>
2023-02-11 15:32:47 +0800

    fix: v14;

--- demo-project/demo.ts
+++ demo-project/demo.ts
@@ -1,33 +1,5 @@
-class Person {}
-
-function factory<T extends new (...args: any[]) => any>(ctor: T): InstanceType<T> {
-	return new ctor()
+function toCamelCase(str: string): string {
+  return str.replace(/-([a-z])/g, (match) => match[1].toUpperCase());
 }
 
-const p = factory(Person) // Person 类型
-
-interface IPerson {
-  name: string
-  age: number
-}
-
-function printPerson(person: IPerson) {
-}
-const kobe = { name: "kobe", age: 30, height: 1.98 }
-printPerson(kobe)
-
-
-class Point<T = number> {
-  x: T
-  y: T
-  constructor(x: T, y: T) {
-    this.x = x
-    this.y = y
-  }
-}
-
-const p1 = new Point(10, 20)
-console.log(p1.x)
-const p2 = new Point("123", "321")
-console.log(p2.x)
-export {}
+console.log(toCamelCase('sdfe-gsderg-rnejtgd'))
